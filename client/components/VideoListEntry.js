4bc3512f7e2d7e98f4d4c3592d053056ba02f362
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-27 15:36:18 -0800

    Adds thumbnail to video list

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -7,7 +7,7 @@ class VideoListEntry extends React.Component { //video Object
   render() {
     return (<div className="video-list-entry">
         <div className="media-left media-middle">
-          <img className="media-object" src="//i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
         </div>
         <div className="media-body">
           <div className="video-list-entry-title">{this.props.video.snippet.title}</div>
