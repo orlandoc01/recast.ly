5b2917c49e9125e4a1183e16ac67f7588457b43a
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-27 15:58:39 -0800

    Adds initial state to App with exampleVideoData

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,27 @@
-var App = () => (
-  <div>
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {videoResults: exampleVideoData};
+  }
+
+
+
+
+  render() {
+    return (<div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer nowPlaying={window.exampleVideoData[0]}/>
+      <VideoPlayer nowPlaying={this.state.videoResults[0]}/>
     </div>
     <div className="col-md-5">
-      <VideoList videoList={window.exampleVideoData}/>
+      <VideoList videoList={this.state.videoResults}/>
     </div>
-  </div>
-);
+  </div>);
+  }
+
+}
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
