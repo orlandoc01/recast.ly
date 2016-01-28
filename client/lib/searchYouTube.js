fb3406c7105100c254a99e78919b504960f854ca
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-27 19:29:37 -0800

    search Youtube now uses a default object and index.html now includes lodash

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,17 +1,17 @@
 var searchYouTube = (options, callback) => {
-
   // TODO
-  $.ajax({
-    url: "https://www.googleapis.com/youtube/v3/search",
-    cache: false,
-    type: 'GET',
-    data: {
+  var defaults = {
       part: 'snippet',
       key: window.YOUTUBE_API_KEY,
       type: "video",
       max: 5,
-      query: "game of thrones"
-    },
+    };
+
+  $.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search",
+    cache: false,
+    type: 'GET',
+    data: _.merge(defaults, options),
     dataType: 'json',
     success: function(data) {
       callback(data);
@@ -25,7 +25,9 @@ var searchYouTube = (options, callback) => {
 
 window.searchYouTube = searchYouTube;
 
-
+var log = function(data) {
+  console.log(data)
+}
 
 // var options = {
 //   key: 'AIzaSyDvEaPTlv9zYqaGE9u-Tw5dq_YyDRKG3Uw',
