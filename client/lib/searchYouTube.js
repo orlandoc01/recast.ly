df6f8bc82b1c21b27580bcba4a532344f8f9423c
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-27 19:20:18 -0800

    Implement basic AJAX request

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,35 @@
 var searchYouTube = (options, callback) => {
+
   // TODO
+  $.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search",
+    cache: false,
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      key: window.YOUTUBE_API_KEY,
+      type: "video",
+      max: 5,
+      query: "game of thrones"
+    },
+    dataType: 'json',
+    success: function(data) {
+      callback(data);
+      console.log(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
+
+
+
+// var options = {
+//   key: 'AIzaSyDvEaPTlv9zYqaGE9u-Tw5dq_YyDRKG3Uw',
+//   max: 5,
+//   query: 'game of thrones theme'
+// };
+// $(document).ready(function(){searchYouTube(options, log);});
