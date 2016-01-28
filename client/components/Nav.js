ee9726962e91498bc9cc419f83655a423ccb7f70
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-28 10:45:59 -0800

    Refactors Nav and Search to be subclasses of React.Component

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,9 +1,18 @@
-var Nav = () => (
-  <nav className="navbar">
-    <div className="col-md-6 col-md-offset-3">
-      <Search />
-    </div>
-  </nav>
-);
+class Nav extends React.Component{
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <nav className="navbar">
+      <div className="col-md-6 col-md-offset-3">
+        <Search />
+      </div>
+    </nav>
+    );
+  }
+};
 
 window.Nav = Nav;
